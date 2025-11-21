c37b38fb8052f54b50fae9bd4b37831c12efcc0f
arnbasten <arnba@Maison_Pro>
2025-11-21 15:27:01 +0100

    suppression de la bannière au click

--- javascript/1-banière/script.js
+++ javascript/1-banière/script.js
@@ -1,1 +1,14 @@
-console.log("tu es connecté !")
+document.querySelector('.btn-success');
+
+const btnSuccess = document.querySelector('.btn-success');
+console.log(btnSuccess);
+
+const cookies = document.querySelector('.cookies');
+console.log(cookies);
+
+btnSuccess.addEventListener('click', function() {
+    console.log('bouton cliqué');
+    cookies.style.opacity = "0";
+
+
+});

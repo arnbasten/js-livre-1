695f8141fa4e814ff89ff1a258cef2a3f030db45
arnbasten <arnba@Maison_Pro>
2026-01-06 20:39:01 +0100

    abonné - dynamisation du bouton  clic icon et clic label indépandant

--- javascript/2-abonné/script_2.js
+++ javascript/2-abonné/script_2.js
@@ -1,6 +1,29 @@
 //Je selectionne et je stocke
-
-console.log("script2 bien chargé");
-
+//icon contient le 1er élément <i> trouvé dans le code
 const icon = document.querySelector('i');
 console.log(icon);
+
+//pour différencier lees chgts innerText du DOM entre icon et label
+const label = document.querySelector('.label');
+console.log(label);
+
+//btnButton contient le 1er élément avec la class btn
+const btnButton = document.querySelector('.subscriber-btn');
+console.log(btnButton);
+
+// je soumets l'icone à une action au clic
+
+icon.addEventListener('click', function(e) {
+    e.stopPropagation();
+    console.log('icon cliquée');
+    icon.classList.toggle('fa-smile-wink');
+    icon.classList.toggle("happy");
+});
+
+btnButton.addEventListener('click', function(){
+    console.log('bouton cliqué');
+    if (label.innerText === "Abonnez-vous"){
+        label.innerText = "Vous êtes abonné";}
+
+    else {(label.innerText = "Abonnez-vous")};
+});

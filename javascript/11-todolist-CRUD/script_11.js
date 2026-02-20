622614e856ae17eeb3d586bc953116044161409d
arnbasten <arnba@Maison_Pro>
2026-02-20 16:16:12 +0100

    todolist - create & read + historique dans le DOM

---
+++ javascript/11-todolist-CRUD/script_11.js
@@ -0,0 +1,32 @@
+//Gérer la création de la tâche "create"
+const form = document.getElementById('form');
+console.log(form);
+const todo = document.getElementById('todo');
+console.log(todo);
+
+//récupération de la valeur input
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log('bouton cliqué');
+    const todoValue = todo.value;
+    console.log(todoValue);
+
+//gérer la lecture de la tâche "read"
+let item = `
+                <div class="item">
+                    <p>${todoValue}</p>
+                    <button class="btn-delete">
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
+                    <button class="btn-archive">
+                        <i class="fas fa-check-circle"></i>
+                    </button>
+                </div>
+            `
+//selectionner et stocker list-items
+const listItems = document.querySelector('.list-items');
+console.log(listItems);
+
+//placer l'item dans listItems
+listItems.insertAdjacentHTML('beforeend', item);
+});

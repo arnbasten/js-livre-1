02f280def7f40ee5cd5a76cc02241be8ba96f893
arnbasten <arnba@Maison_Pro>
2026-02-23 11:50:11 +0100

    démo local storage

---
+++ javascript/12a-démo-localStorage/script_12a.js
@@ -0,0 +1,7 @@
+//Stockage sous la forme clé / valeur
+localStorage.setItem('name', 'Martin');
+
+//Créer un espace local storage
+const stockage = window.localStorage;
+console.log(stockage.getItem('name'));
+
